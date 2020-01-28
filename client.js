473d92807919ed27fded08acea9d73edfd2e3547
Kit Kogan <kkogan.dev@gmail.com>
2020-01-28 16:22:53 -0600

    Update client.js

--- client.js
+++ client.js
@@ -34,15 +34,22 @@ const employees = [
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 function pay(employees) {
-  for (employee of employees) {
-    if (employee.reviewRating > 2) {
-      return "will recieve bonus";
-    } else if (employee.reviewRating <= 2) {
-     return "will not recieve bonus";
+  for (i = 0; i < employees.length; i++) {
+    const employee = employees[i];
+    if (employee.reviewRating < 2) {
+     return bonusPercentage = 0;
+    
+    } else if (employee.reviewRating = 3) {
+      return bonusPercentage = .04;
+    } else if (employee.reviewRating = 4) {
+      return bonusPercentage = .06;
+    } else if (employee.reviewRating = 5){
+      return bonusPercentage = .10;
+
     }// end conditional
   }//end for of
 }//end pay
-  console.log(employees.reviewRating);
+  //console.log(employees.reviewRating);
 //end pay
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -53,4 +60,5 @@ function pay(employees) {
 
 //console.log( employees );
 //console.log(pay(employees));
-console.log(pay('Atticus', 3));
+console.log(pay(employees));
+//console.log(bonusPercentage[i]);
